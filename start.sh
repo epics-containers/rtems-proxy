444c4db981fd3a4250ba90621217472213d30506
Giles Knap <gilesknap@gmail.com>
2024-03-16 21:41:37 +0000

    tidy startup

--- start.sh
+++ start.sh
@@ -8,27 +8,29 @@ set -xe
 #
 # Each RTEMS IOC will mount its sub-folder as /epics - meaning that the file
 # paths are identical to native x86 EPICS IOCs
-export PVC_ROOT=/nfsv2tftp
+#
+# This can be configured via globalenv in the beamline shared helm values.yaml
+export RTEMS_ROOT=${RTEMS_ROOT:-/nfsv2tftp}
 
-# start.sh requires these environment variables to be set
+# epics/ioc/start.sh requires these environment variables to be set
 export EPICS_ROOT=/epics
 export SUPPORT=${EPICS_ROOT}/support
 export IOC=${EPICS_ROOT}/ioc
 
-if [ ! -d ${PVC_ROOT} ]; then
+if [ ! -d ${RTEMS_ROOT} ]; then
     echo "ERROR: No PVC folder found."
     # make a folder for testing outside of the cluster
-    mkdir -p ${PVC_ROOT}
+    mkdir -p ${RTEMS_ROOT}
 fi
 
 # generate the runtime assets
 /epics/ioc/start.sh
 
 # copy the runtime assets into the shared volume
-cp -r /epics/ioc ${PVC_ROOT}
-cp -r /epics/runtime ${PVC_ROOT}
+cp -r /epics/ioc ${RTEMS_ROOT}
+cp -r /epics/runtime ${RTEMS_ROOT}
 # make the boot file path shorter
-ln -s ${PVC_ROOT}/epics/ioc/bin/*/ioc.boot ${PVC_ROOT}
+ln -s ${RTEMS_ROOT}/epics/ioc/bin/*/ioc.boot ${RTEMS_ROOT}
 
 # keep the container running ...
 while true; do
