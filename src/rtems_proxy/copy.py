eaa3d0677ae9aec5b65669c036d25d7e97c23c4d
Giles Knap <giles.knap@diamond.ac.uk>
2025-01-24 15:58:52 +0000

    handle protocol files

--- src/rtems_proxy/copy.py
+++ src/rtems_proxy/copy.py
@@ -16,7 +16,7 @@ def copy_rtems():
     # root of pvc mount into which we copy the IOC files for the RTEMS IOC to access
     root = GLOBALS.RTEMS_TFTP_PATH
     # root of the path that the RTEMS IOC expects to find the IOC files
-    RTEMS_TFTP_PATH = Path("/iocs") / GLOBALS.IOC_GROUP / GLOBALS.IOC_NAME
+    RTEMS_TFTP_PATH = Path("/iocs") / GLOBALS.IOC_NAME
     # where to copy the Generic IOC folder to (at present only holds the dbd folder)
     ioc_dest = root / "ioc"
     # where to copy the generated runtime assets to (st.cmd and ioc.db)
@@ -36,8 +36,18 @@ def copy_rtems():
     shutil.copy(bin_rtems_src, bin_rtems_dest)
     shutil.copytree(GLOBALS.RUNTIME, dest_runtime, dirs_exist_ok=True)
 
+    protocol_folder = RTEMS_TFTP_PATH / "protocol"
+    protocol_files = GLOBALS.SUPPORT.glob("**/*.proto*")
+    for proto_file in protocol_files:
+        dest = protocol_folder / proto_file.name
+        shutil.copy(proto_file, dest)
+
     # because we moved the ioc files we need to fix up startup script paths
     startup = dest_runtime / "st.cmd"
     cmd_txt = startup.read_text()
     cmd_txt = re.sub("/epics/", f"{str(RTEMS_TFTP_PATH)}/", cmd_txt)
+    # also fix up the protocol path to point to protocol_folder
+    cmd_txt = (
+        cmd_txt + f'\nepicsEnvSet("STREAM_PROTOCOL_PATH", "{str(protocol_folder)}")\n'
+    )
     startup.write_text(cmd_txt)
